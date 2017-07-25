41411a4d2e546d66e2d5a68fefa212c4af2f6fc3
Simon <superpenshine@gmail.com>
2017-07-25 16:29:25 -0700

    2 test deleted

--- tests/test_mothership_basic.py
+++ tests/test_mothership_basic.py
@@ -5,6 +5,7 @@ from mothership.base import MothershipServer
 from workers.basic_worker import BasicUserParseWorker
 
 class TestMothershipBasic(unittest.TestCase):
+	
     def test_basic_mothership_listen(self):
         """
         Purpose: Test regular running of worker
@@ -18,10 +19,5 @@ class TestMothershipBasic(unittest.TestCase):
         # Can't connect to mother, so should raise ConnectionRefusedError, but should run everything else
         self.assertRaises(Exception, mothership.run)
 
-    def test_basic_mothership_contact(self):
-    	mothership = MothershipServer()
-    	mothership.run()
-    	worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
-    	self.assertRaises(Exception, worker.run)
 
 
