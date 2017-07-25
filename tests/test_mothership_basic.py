250e5d945c59f68d2a876e0499f3720aa87caf3c
Simon <superpenshine@gmail.com>
2017-07-25 16:53:54 -0700

    7/25 4:53

--- tests/test_mothership_basic.py
+++ tests/test_mothership_basic.py
@@ -1,11 +1,14 @@
 
 import unittest
+import time
 
 from mothership.base import MothershipServer
 from workers.basic_worker import BasicUserParseWorker
 
 class TestMothershipBasic(unittest.TestCase):
-	
+	def sleep(mothership):
+		time.sleep(2)
+
     def test_basic_mothership_listen(self):
         """
         Purpose: Test regular running of worker
@@ -15,9 +18,10 @@ class TestMothershipBasic(unittest.TestCase):
         :return:
         """
         mothership = MothershipServer()
-
+        threading.Thread(target=self.sleep, args=(mothership)).start()
         # Can't connect to mother, so should raise ConnectionRefusedError, but should run everything else
         self.assertRaises(Exception, mothership.run)
+
 
 
 
