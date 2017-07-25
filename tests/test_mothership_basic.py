70872ea453f6ad36c183b5a857fa7d55dd887fdf
Simon <superpenshine@gmail.com>
2017-07-25 16:24:39 -0700

    2 tests added

--- tests/test_mothership_basic.py
+++ tests/test_mothership_basic.py
@@ -2,6 +2,26 @@
 import unittest
 
 from mothership.base import MothershipServer
+from workers.basic_worker import BasicUserParseWorker
 
 class TestMothershipBasic(unittest.TestCase):
-    pass
+    def test_basic_mothership_listen(self):
+        """
+        Purpose: Test regular running of worker
+        Expectation: startup system, hit the reddit user and parse the data, fail to send to mothership (exception)
+
+        :precondition: Mothership server not running
+        :return:
+        """
+        mothership = MothershipServer()
+
+        # Can't connect to mother, so should raise ConnectionRefusedError, but should run everything else
+        self.assertRaises(Exception, mothership.run)
+
+    def test_basic_mothership_contact(self):
+    	mothership = MothershipServer()
+    	mothership.run()
+    	worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
+    	self.assertRaises(Exception, worker.run)
+
+
