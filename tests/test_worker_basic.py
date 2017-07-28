71a4776555ffccf6717b34d21988ae5c8af24ab6
Simon <superpenshine@gmail.com>
2017-07-27 18:54:39 -0700

    v2.0

--- tests/test_worker_basic.py
+++ tests/test_worker_basic.py
@@ -49,3 +49,52 @@ class TestWorkerBasic(unittest.TestCase):
         len_to_crawl_after = len(worker.to_crawl)
 
         self.assertEqual(len_to_crawl_after, len_to_crawl_before)
+        
+    def test_worker_add_links_to_crawled_1(self):
+        """
+        Purpose: Test if cur_links is added correctly
+        Expectation: added one urls by calling the add_links funcition, the add_links increased to 2
+
+        :return:
+        """
+        worker = None 
+        worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
+        worker.to_crawl = ["https://www.reddit.com/user/Chrikelnel"]
+        worker.add_links(["https://www.google.ca/"])
+        
+        self.assertRaises(Exception, worker.run)
+
+        self.assertEqual(worker.cur_links, 2)
+
+
+    def test_worker_add_links_to_crawled_2(self):
+        """
+        Purpose: Test if cur_links will be added if the duplicated link is added accidently
+        Expectation: added two urls, the add_links increased to 2, not 3 (filtered out when append to the to_crawl list)
+
+        :return:
+        """
+        worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
+        worker.to_crawl = ["https://www.reddit.com/user/Chrikelnel"]
+        worker.add_links(["https://www.google.ca/"])
+        worker.add_links(["https://www.google.ca/"])
+        self.assertRaises(Exception, worker.run)
+        self.assertEqual(worker.cur_links, 2)
+
+
+    def test_basic_connection_with_mothership_listen(self):
+        """
+        Purpose: Test regular listen function of server
+        Expectation: startup system, hit the reddit user and parse the data, send to mothership and success.
+
+        :precondition: Mothership server running
+        :return:
+        """
+        mothership = MothershipServer()
+        worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
+        # Can't connect to mother, so should raise ConnectionRefusedError, but should run everything else
+        self.assertRaises(Exception, worker.run)
+
+
+
+
